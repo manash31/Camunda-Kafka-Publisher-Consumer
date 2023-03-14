99e7ebfaddacf1c284067546b98cab32c2b9d39e
manash31@gmail.com <manash31@gmail.com>
2023-03-14 01:21:42 -0400

    Changed Topic Name

--- src/main/java/com/manash/SpringBoot/KafkaApp/Consumer.java
+++ src/main/java/com/manash/SpringBoot/KafkaApp/Consumer.java
@@ -1,5 +1,4 @@
 package com.manash.SpringBoot.KafkaApp;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
